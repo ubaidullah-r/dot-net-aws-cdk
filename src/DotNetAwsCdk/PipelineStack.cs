00b82aa632bdaf3eb426dccd1bbc07529cf82f39
Ubaid Ullah <ubaidullah4211@gmail.com>
2022-12-27 21:01:53 +0500

    MESSAGE

--- src/DotNetAwsCdk/PipelineStack.cs
+++ src/DotNetAwsCdk/PipelineStack.cs
@@ -1,6 +1,9 @@
 using Amazon.CDK;
-using Constructs;
 using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.Pipelines;
+using Constructs;
 
 namespace CdkWorkshop
 {
@@ -8,10 +11,28 @@ namespace CdkWorkshop
     {
         public WorkshopPipelineStack(Construct parent, string id, IStackProps props = null) : base(parent, id, props)
         {
-            // Pipeline code goes here
-             var repo = new Repository(this, "WorkshopRepo", new RepositoryProps
+            // Creates a CodeCommit repository called 'WorkshopRepo'
+            var repo = new Repository(this, "WorkshopRepo", new RepositoryProps
             {
                 RepositoryName = "WorkshopRepo"
+            });
+
+            // The basic pipeline declaration. This sets the initial structure
+            // of our pipeline
+            var pipeline = new CodePipeline(this, "Pipeline", new CodePipelineProps
+            {
+                PipelineName = "WorkshopPipeline",
+
+                // Builds our source code outlined above into a could assembly artifact
+                Synth = new ShellStep("Synth", new ShellStepProps
+                {
+                    Input = CodePipelineSource.CodeCommit(repo, "main"),  // Where to get source code to build
+                    Commands = new string[] {
+                        "npm install -g aws-cdk",
+                        "sudo apt-get install -y dotnet-sdk-3.1", // Language-specific install cmd
+                        "dotnet build"  // Language-specific build cmd
+                    }
+                }),
             });
         }
     }

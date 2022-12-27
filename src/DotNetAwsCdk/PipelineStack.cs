99b6b1e1ccddd18b615db6ea69015f4a590944a5
Ubaid Ullah <ubaidullah4211@gmail.com>
2022-12-27 23:47:22 +0500

    updating  shell script in pipelineStack.cs again4

--- src/DotNetAwsCdk/PipelineStack.cs
+++ src/DotNetAwsCdk/PipelineStack.cs
@@ -31,6 +31,7 @@ namespace CdkWorkshop
                         "sudo apt-get update",
                         "sudo apt-get install -y dotnet-sdk-3.1",
                         "cd src",
+                        "cd ..",
                         "dotnet build"  // Language-specific build cmd
                     }
                 }),
